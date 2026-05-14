1c2eb06e2e85504665e168b7d8e9949a2c3cbb51
Raul <mazmitiao@miumg.edu.gt>
2026-05-13 23:10:57 -0600

    backend mongodb funcionando

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,18 @@
+import os
+from dotenv import load_dotenv
 from pymongo import MongoClient
 
-# Conexión MongoDB local
-client = MongoClient("mongodb://localhost:27017")
+# Cargar variables .env
+load_dotenv()
+
+# Leer variable de entorno
+MONGO_URI = os.getenv(
+    "MONGO_URI",
+    "mongodb://localhost:27017"
+)
+
+# Conexión MongoDB
+client = MongoClient(MONGO_URI)
 
 # Base de datos
 db = client["robot_logs"]
